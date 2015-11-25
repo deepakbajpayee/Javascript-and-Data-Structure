a3fc5bb8b7c19125555e345525a1359109c31bbb
unknown <I311063@INLN50866789A.apj.global.corp.sap>
2015-11-25 22:28:16 +0530

    A program to calculate total number of nodes in a tree

---
+++ Tree/Size_of_tree.js
@@ -0,0 +1,19 @@
+
+
+function size(root){
+	if(root==undefined) return 0;
+	var leftHeight = size(root.left);
+	var rightHeight = size(root.right);
+	return 1+leftHeight+rightHeight;
+}
+
+var tree = new Tree();
+tree.put(3);
+tree.put(1);
+tree.put(2);
+tree.put(7);
+tree.put(9);
+tree.put(4);
+tree.put(5);
+var size = size(tree.getRoot());
+console.log("Total number of nodes : "+size);
