56c3c63af3faed1161364d9a05c24f389cd63924
SlimDeepak <I311063@INLN50866789A.apj.global.corp.sap>
2015-11-26 19:38:53 +0530

    Add Dictionary and LinkedList2 code

---
+++ List/Dictionary.js
@@ -0,0 +1,28 @@
+function Dictionary(){
+	this.data = new Array();
+}
+
+Dictionary.prototype.add = function(key,value){
+	this.data[key] = value;
+}
+
+Dictionary.prototype.find = function(key){
+	return this.data[key];
+}
+
+Dictionary.prototype.remove = function(key){
+	delete this.data[key];
+}
+
+Dictionary.prototype.showAll = function(){
+	var keys = Object.keys(this.data);
+	for(var i=0;i<keys.length;i++){
+		console.log(keys[i] + " - >" + this.data[keys[i]]);
+	}
+}
+
+var dictionary = new Dictionary();
+dictionary.add("1","Lucky");
+dictionary.add("2","Tucky");
+console.log(dictionary.find("1"));
+dictionary.showAll();
---
+++ List/LinkedList2.js
@@ -0,0 +1,58 @@
+function Node(element){
+	this.element = element;
+	this.next = null;
+}
+
+function LList(){
+	this.head = new Node("head");
+	this.find = find;
+	this.insert = insert;
+	this.display = display;
+	this.findPrevious = findPrevious;
+	this.remove = remove;
+}
+
+function find(item){
+	var current = this.head;
+	while(current.element!=item){
+		current = current.next;
+	}
+	return current;
+}
+
+function insert(newElement,item){
+	var node = new Node(newElement);
+	var current =this.find(item);
+	node.next = current.next;
+	current.next = node;
+}
+
+function display(){
+	var current = this.head;
+	while(!(current.next==null)){
+		console.log(current.next.element);
+		current = current.next;
+	}
+}
+
+function findPrevious(item){
+	var current = this.head;
+	while(!(current.next==null) && (current.next.element != item)){
+		current = current.next;
+	}
+	return current;
+}
+
+function remove(item){
+	var prevNode = this.findPrevious(item);
+	if(!(prevNode.next==null)){
+		prevNode.next = prevNode.next.next;
+	}
+}
+
+
+var citites = new LList();
+citites.insert("Delhi","head");
+citites.insert("Mumbai","Delhi");
+citites.remove("Delhi");
+citites.display();
