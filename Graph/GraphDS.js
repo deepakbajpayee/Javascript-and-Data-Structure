c92a6b36a67cf032514575649a0c2e1c04309aff
RealSlimDeepak <I311063@INLN50866789A.apj.global.corp.sap>
2015-12-05 20:55:50 +0530

    Graph Data Structure :Revealing Module Pattern

--- Graph/GraphDS.js
+++ Graph/GraphDS.js
@@ -1,9 +1,74 @@
 /**
 This program represent a graph , by storing it in adjacency list fashion.
 The graph is undirected . If we say there is a path (A-B).It means there is a path
-from A to B and B to A as well
+from A to B and B to A as well.
 **/
 
+
+/*************************************************************************************************************************/
+/*	This Graph data structure follows Revealing Module pattern. I used this pattern so that I can
+   export this module , and in other file I can use it.*/
+
+var GraphDS = (function(){
+	var vertex =  0;
+	var adj = [];
+
+	var instantiate = function(v){
+		vertex = v;
+		for(var i=0;i<=vertex;i++){
+			adj.push([]);
+		}
+	}
+
+	var addEdge = function(u,v){
+		adj[u].push(v);
+		adj[v].push(u);
+	}
+
+	var vertices = function(){
+		return vertex;
+	}
+
+	var adjArray = function(i){
+		return adj[i];
+	}
+	return{
+		instantiate:instantiate,
+		addEdge : addEdge,
+		vertices : vertices,
+		adjArray : adjArray
+	}
+})();
+
+
+/*
+//This is the test code to check function of graph.
+
+GraphDS.instantiate(10);
+GraphDS.addEdge(1,2);
+GraphDS.addEdge(1,5);
+GraphDS.addEdge(2,4);
+GraphDS.addEdge(3,7);
+GraphDS.addEdge(4,8);
+GraphDS.addEdge(4,9);
+
+for(var i=0;i<GraphDS.vertices();i++){
+	GraphDS.adjArray(i).forEach(function(element,index,array){
+		console.log(i+" - > "+element);
+	});	
+}
+
+*/
+//If you don't use Node.js You have to implement this file to my other Graph programs.
+module.exports = GraphDS;
+
+
+
+/*******************************************************************************************************************************/
+//This is pseudoclassical pattern way of creating Graph.
+
+
+/*
 function Graph(v){
 	this.V = v;
 	this.adj = [];
@@ -23,7 +88,6 @@ function Graph(v){
 	this.adjArray = function(i){
 		return this.adj[i];
 	}
-
 }
 
 var graph = new Graph(10);
@@ -38,5 +102,6 @@ for(var i=0;i<graph.vertices();i++){
 	graph.adjArray(i).forEach(function(element,index,array){
 		console.log(i+" - > "+element);
 	});	
-}
+}*/
 
+/*****************************************************************************************************************************/
